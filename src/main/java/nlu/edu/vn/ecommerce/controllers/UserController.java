4cc1d68f979352651889ec913e025fb4a834b3a5
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-03-31 19:18:19 +0700

    up ++

--- src/main/java/nlu/edu/vn/ecommerce/controllers/UserController.java
+++ src/main/java/nlu/edu/vn/ecommerce/controllers/UserController.java
@@ -1,33 +1,35 @@
 package nlu.edu.vn.ecommerce.controllers;
 
+import nlu.edu.vn.ecommerce.dto.UpdateUserDTO;
 import nlu.edu.vn.ecommerce.dto.UserDTO;
+import nlu.edu.vn.ecommerce.exception.MyException;
+import nlu.edu.vn.ecommerce.exception.ResponseObject;
 import nlu.edu.vn.ecommerce.models.User;
 import nlu.edu.vn.ecommerce.repositories.UserRepository;
+import nlu.edu.vn.ecommerce.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
     @Autowired
-    AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private IUserService iUserService;
 
     @GetMapping("/me")
     public ResponseEntity<?> me(@AuthenticationPrincipal User user) {
-        if(user != null){
+        if (user != null) {
             return ResponseEntity.ok().body(UserDTO.from(user));
-        }
-        else{
+        } else {
             return ResponseEntity.ok().body(null);
         }
     }
@@ -36,5 +38,17 @@ public class UserController {
     @PreAuthorize("#user.id == #id")
     public ResponseEntity<?> me(@AuthenticationPrincipal User user, @PathVariable String id) {
         return ResponseEntity.ok().body(UserDTO.from(userRepository.findById(id).orElseThrow()));
+    }
+
+    @PutMapping("/update/{userId}")
+    @PreAuthorize("#user.id == #userId")
+    public ResponseEntity<?> updateUser(@AuthenticationPrincipal User user,@PathVariable("userId")String userId, @RequestBody UpdateUserDTO updatedUser) {
+        boolean result = iUserService.updateUser(userId, updatedUser);
+
+        if (result) {
+            return ResponseEntity.ok().body(new MyException(HttpStatus.OK,"Thành công"));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/nlu/edu/vn/ecommerce/repositories/UserRepository.java
+++ src/main/java/nlu/edu/vn/ecommerce/repositories/UserRepository.java
@@ -2,10 +2,14 @@ package nlu.edu.vn.ecommerce.repositories;
 
 import nlu.edu.vn.ecommerce.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
+    Optional<User> findById(String id);
     boolean existsByUsername(String s);
     boolean existsByEmail(String s);
 }
